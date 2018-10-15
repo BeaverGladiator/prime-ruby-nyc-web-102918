5794935f0a2c0fd36d64a25ae167ec3169ab6f8d
Matthew Migliore <mmigliore311@gmail.com>
2018-10-15 13:38:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,5 +5,10 @@ def prime?(integer)
   divisible_integer_amount = 0
   
   numbers_array.each do |number|
-    if integer / number 
+    if (integer/number).is_a? Integer
+      divisible_integer_amount += 1 
+    end 
+  end
+  
+  
 end
